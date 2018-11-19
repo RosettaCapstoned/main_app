ca61ed9a3171f65acd68a92cee81116044967603
Kevin Hu <pkvinhu@gmail.com>
2018-11-19 16:12:08 -0500

    edit auth

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
-import ReactDOM, { render } from 'react-dom';
+import React from 'react';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './components/App.js'
