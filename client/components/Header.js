7e53c24c76584b3c9971c8532ead8f80191ae64f
Kevin Hu <32854255+pkvinhu@users.noreply.github.com>
2018-11-20 20:31:17 -0500

    Enzyme tests (#6)
    * Google OAuth 2 set up
    
    * edit auth
    
    * secured auth key and secret
    
    * added basic enzyme testing with jest
    
    * more tests on FE

---
+++ client/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Typography } from '@material-ui/core';
+
+class Header extends Component {
+
+  render(){
+
+  	return (
+  	  <div>
+  	    <Typography variant='display1' color='textPrimary'>Rosetta Capstone</Typography>
+  	  </div>
+  	)
+  }
+}
+
+export default Header;
--- client/testing/components.spec.js
+++ client/testing/components.spec.js
@@ -2,17 +2,56 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import { createShallow } from '@material-ui/core/test-utils';
 import { Typography } from '@material-ui/core';
+import { HashRouter, MemoryRouter } from 'react-router-dom';
 import App from '../components/App';
+import LandingPage from '../components/LandingPage';
+import Login from '../components/Login';
+import Header from '../components/Header';
 
-describe('<App /> Component', () => {
+
+describe('<Header /> Component', () => {
   it('renders a div', () => {
-  	const wrapper = shallow(<App />);
+  	const wrapper = shallow(<Header />);
     expect(wrapper.find('div')).toHaveLength(1);
   });
 
   it('has a material-ui <Typography /> component that says Rosetta Capstone', () => {
   	let shallow = createShallow();
-  	const wrapper = shallow(<App />);
+  	const wrapper = shallow(<Header />);
   	expect(wrapper.contains([<Typography variant='display1' color='textPrimary'>Rosetta Capstone</Typography>])).toBe(true);
   });
+});
+
+describe('<App /> Component', () => {
+
+  it('has a router that can take you to the landing page', () => {
+  	const wrapper = mount(
+  	  <MemoryRouter initialEntries={[ '/' ]}>
+  	    <App />
+  	  </MemoryRouter>)
+
+  	// expect(wrapper.find(Header)).toHaveLength(1);
+  	expect(wrapper.find(LandingPage)).toHaveLength(0);
+  })
+
+  it('has a router that can take you to the login form', () => {
+  	const wrapper = mount(
+  	  <MemoryRouter initialEntries={[ '/login']}>
+  	    <App />
+  	  </MemoryRouter>)
+
+  	expect(wrapper.find(Header)).toHaveLength(1);
+  	expect(wrapper.find(Login)).toHaveLength(0);
+  })
+
+  it('has a router that always show header', () => {
+  	const wrapper = mount(
+  	  <MemoryRouter initialEntries={[ '/random']}>
+  	    <App />
+  	  </MemoryRouter>)
+
+  	expect(wrapper.find(Header)).toHaveLength(1);
+  	expect(wrapper.find(LandingPage)).toHaveLength(0);
+  	expect(wrapper.find(Login)).toHaveLength(0);
+  })
 });
