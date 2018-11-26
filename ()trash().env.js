42c49ad60973a1d1198f3bb25fe30d96603779ad
Daniel Seeley <Daniel.h.seeley@gmail.com>
2018-11-25 21:19:27 -0500

    removed accidentally uploaded files

--- ()trash().env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    clientID:     '126317270450-sd9fvuco3pu5e5il4e1tj043gbc9l9t1.apps.googleusercontent.com',
-    clientSecret: 'GhdPMS5SPPv7zaQDexTTo-xv',
-    callbackURL: 'http://localhost:3000/auth/google/callback'
-    // apiKey:'ab446ae790d628e3b493ef90'
-}
