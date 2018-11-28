d95a43fdaeae8c90e770890b9d4ae7ad1b79118c
Kazniyaz Kabyldenov <kazniyazkz@gmail.com>
2018-11-27 20:09:23 -0500

    Password hashing + some security

--- client/components/App.js
+++ client/components/App.js
@@ -4,46 +4,48 @@ import { connect } from 'react-redux';
 import LandingPage from './LandingPage';
 import Login from './Login';
 import Header from './Header';
-import Connection from './Connection'
-import SignUp from './SignUp'
+import Connection from './Connection';
+import SignUp from './SignUp';
 import { checkOAuthToken } from '../store/auth';
 
 class App extends Component {
-
-  componentDidMount(){
-  	const { checkOAuthToken } = this.props;
-  	checkOAuthToken();
+  componentDidMount() {
+    const { checkOAuthToken } = this.props;
+    checkOAuthToken();
   }
 
   render() {
-  	const renderLogin = ({ history }) => <Login history={history} />;
-  	const { token } = this.props;
-  	console.log('Token rendered from App: ', token);
-  	return (
-  	<Router>
-  	  <div>
-  	      <Route component={Header}/>
-			<Switch>
-				<Route exact path='/classroom' component={Connection}/>
-				<Route exact path='/login' render={renderLogin}/>
-				<Route exact path='/signup' component={SignUp}/>
-				<Route exact path='/' component={LandingPage}/>
-				<Route path='/auth/google'/>
-			</Switch>
-  	  </div>
-    </Router>
-  	)
+    const renderLogin = ({ history }) => <Login history={history} />;
+    const { token } = this.props;
+    console.log('Token rendered from App: ', token);
+    return (
+      <Router>
+        <div>
+          <Route component={Header} />
+          <Switch>
+            <Route exact path="/classroom" component={Connection} />
+            <Route exact path="/login" render={renderLogin} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/auth/google" />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
 const mapStateToProps = ({ auth }) => {
-  console.log(auth)
+  //console.log(auth)
   return {
-  	token: auth ? auth.auth : false
-  }
-}
+    token: auth ? auth.auth : false,
+  };
+};
 const mapDispatchToProps = dispatch => ({
   checkOAuthToken: () => dispatch(checkOAuthToken()),
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- server/api/auth.js
+++ server/api/auth.js
@@ -1,47 +1,48 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const jwt2 = require('jwt-simple');
 const User = require('../db/Models/User');
 module.exports = router;
 
 router.post('/signup', (req, res, next) => {
-  const { firstName, lastName, password, email } = req.body
+  const { firstName, lastName, password, email } = req.body;
+  const signedPassword = jwt2.encode(password, process.env.JWT_SECRET);
   User.create({
     firstName,
     lastName,
-    password,
     email,
-    role: 'Student'
+    role: 'Student',
+    password: signedPassword,
   })
-  .then(user => {
-    res.send(user)
-  })
-  .catch(next)
-
-})
+    .then(user => {
+      res.send(user);
+    })
+    .catch(next);
+});
 
 router.post('/', (req, res, next) => {
-  User.findOne({ 
+  const signedPassword = jwt2.encode(req.body.password, process.env.JWT_SECRET);
+  User.findOne({
     where: {
-      email: req.body.email, 
-      password: req.body.password
-    } 
+      email: req.body.email,
+      password: signedPassword,
+    },
   })
-  .then( user => {
-    if (!user) {
-      return next({ status: 401 })
-    }
-    const token = jwt.sign({ id: user.id }, 
-                process.env.JWT_SECRET)
-    console.log(token);
-    res.send({ token })
-  })
-  .catch(next)
-})
+    .then(user => {
+      if (!user) {
+        return next({ status: 401 });
+      }
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+      console.log(token);
+      res.send({ token });
+    })
+    .catch(next);
+});
 
-router.get('/', (req, res, next)=> {
-  if (!req.user){
-    return next({ status: 401 })
+router.get('/', (req, res, next) => {
+  if (!req.user) {
+    return next({ status: 401 });
   }
   res.status(200).send(req.user);
-})
+});
