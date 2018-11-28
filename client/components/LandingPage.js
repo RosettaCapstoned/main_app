8cf144b50c5102ee9d0a0da561b27c746e3b5d30
Kevin Hu <pkvinhu@gmail.com>
2018-11-28 11:33:30 -0500

    edited landing page, added languageTo select

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Divider } from '@material-ui/core';
+import AboutUs from './AboutUs';
+import WhatWeDo from './WhatWeDo';
 
 class LandingPage extends Component {
   
   render(){
 
   	return (
-  	  <div>
+  	  <div className="landingContainer">
+  	    <div className="sectionContainer">
 				<Typography variant='display4' 
   	  						color='textPrimary' 
+  	  						align="center"
   	  	  					className="footerLogo"
   	  	  					style={{fontFamily: 'Georgia, serif'}}>
 					 Rosetta Capstone
 				</Typography>
+				<Typography variant='h3' 
+  	  						color='secondary' 
+  	  						align="center"
+  	  	  					className="footerLogo"
+  	  	  					style={{fontFamily: 'Georgia, serif'}}>
+					 Bringing English to you
+				</Typography>
+		  </div>
+		  <Divider />
+		<div className="sectionContainer">
+	      <AboutUs />
+	    </div>
+	    <Divider />
+	    <div className="sectionContainer">
+	      <WhatWeDo />
+	    </div>
+	    <Divider />
   	  </div>
   	)
   }
