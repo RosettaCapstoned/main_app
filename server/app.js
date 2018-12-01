5fcfcbd5622e3fde21a8ab4ef4c4bf1dc933b265
Daniel Seeley <Daniel.h.seeley@gmail.com>
2018-12-01 16:56:53 -0500

    api key fix

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ const path = require('path');
 const passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20');
 const User = require('./db/Models/User');
-const googleKey = require('./env');
+// const googleKey = require('./env');
+// const googleKey = process.env.GOOGLE_KEY;
 const cookieSession = require('cookie-session');
 const { userRouter, authRouter, translateRouter, roomRouter, messagesRouter } = require('./api');
 const { sync, seed } = require('./db/');
@@ -15,7 +16,7 @@ const jwt = require('jsonwebtoken');
 //Translation API
 const translate = require('translate');
 translate.engine = 'google';
-translate.key = googleKey.apiKey || process.env.apiKey
+translate.key = process.env.GOOGLE_KEY;
 
 //Wrapping server in socket.io instance
 const server = require('http').Server(app);
